a6b27339006b0005768e16e9937645f139f3b941
Mvrcus <marcuswilson.22@gmail.com>
2023-09-13 12:12:25 -0700

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,8 +1,12 @@
 <?php
+$host = "localhost:3306";
+$username = "kwamecasey_hk2aftfsp8a"; // Replace with your MySQL username
+$password = "nKb3Z0Lp9$"; // Replace with your MySQL password
+$database_name = "kwamecasey";
 
-    $database= new mysqli("localhost:3306","kwamecasey_hk2aftfsp8a","","kwamecasey");
-    if ($database->connect_error){
-        die("Connection failed:  ".$database->connect_error);
-    }
+$database = new mysqli($host, $username, $password, $database_name);
 
+if ($database->connect_error) {
+    die("Connection failed: " . $database->connect_error);
+}
 ?>
