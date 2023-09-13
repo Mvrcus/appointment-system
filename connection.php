3841945d2697156107191f9edd01eacdcf8d2a34
Mvrcus <marcuswilson.22@gmail.com>
2023-09-13 12:09:54 -0700

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,6 +1,6 @@
 <?php
 
-    $database= new mysqli("localhost","root","","edoc");
+    $database= new mysqli("localhost:3306","kwamecasey_hk2aftfsp8a","","kwamecasey");
     if ($database->connect_error){
         die("Connection failed:  ".$database->connect_error);
     }
